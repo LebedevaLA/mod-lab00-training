6eeb38a9354b8c9d570404de9b05cef4a4b64c17
LebedevaLA <alldifferent.6489@gmail.com>
2025-03-10 20:31:39 +0300

    Изменение fun.cpp main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+using namespace std;
 
 int main() {
+    cout << power(1, 3) << endl;
+    cout << power(2, 3) << endl;
+    cout << power(44, 1) << endl;
+    cout << power(11, 2) << endl;
     return 0;
 }
